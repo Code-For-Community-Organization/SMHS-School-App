91b4b855e175778d6ae09da8e48f96d340ca61f9
Jevon Mao <woodburyjevonmao@gmail.com>
2021-05-12 12:47:58 -0700

    Fix news image url parsing bug

---
+++ Sources/Shared/Views/NewsView/NewsViewViewModel.swift
@@ -0,0 +1,76 @@
+//
+//  NewsViewViewModel.swift
+//  SMHSSchedule
+//
+//  Created by Jevon Mao on 5/10/21.
+//
+
+import Foundation
+import Combine
+import SwiftyXMLParser
+import SwiftSoup
+
+class NewsViewViewModel: ObservableObject {
+    var cancellable: AnyCancellable?
+    @Published(key: "newsEntries") var newsEntries = [NewsEntry]()
+    @Published var isLoading: Bool = true
+    init(newsEntries: [NewsEntry]? = nil) {
+        fetchXML()
+    } 
+    
+    func fetchXML() {
+        let url = URL(string: "https://www.smhs.org/fs/post-manager/boards/37/posts/feed")!
+        cancellable = URLSession.shared.dataTaskPublisher(for: url)
+            .retry(2)
+            .receive(on: DispatchQueue.main)
+            .sink(receiveCompletion: {_ in}){data, response in
+                guard let rawText = String(data: data, encoding: .utf8) else {return}
+                self.newsEntries = self.parseXML(for: rawText)
+                print(self.newsEntries)
+            }
+
+    }
+    
+    func parseXML(for rawText: String) -> [NewsEntry] {
+        do {
+            let xml = try XML.parse(rawText)
+            var newsEntries = [NewsEntry]()
+            for feed in xml["feed", "entry"] {
+                guard let title = feed["title"].text,
+                      let author = feed["author", "name"].text?.trimmingCharacters(in: .whitespacesAndNewlines),
+                      let image = parseHTMLImgTag(for: feed["content"].text ?? ""),
+                      let articleURL = URL(string: feed["link"].attributes["href"] ?? "") else {continue}
+                newsEntries.append(.init(title: title, author: author, imageURL: image, articleURL: articleURL))
+            }
+            return newsEntries
+        }
+        catch {
+            print("Parse XML failed with error: \(error)")
+            return []
+        }
+   
+    }
+    
+    func parseHTMLImgTag(for html: String) -> URL? {
+        do {
+            let parsed = try SwiftSoup.parse(html)
+            let img = try parsed.select("img").first()
+            let urlString = try img?.attr("src")
+            return URL(string: urlString ?? "")
+        }
+        catch Exception.Error(let type, Message: let message) {
+            print("Error of type \(type), \(message)")
+        }
+        catch {
+            print(error)
+        }
+        return nil
+    }
+}
+
+extension NewsViewViewModel {
+    static let sampleNewsViewViewModel = NewsViewViewModel(newsEntries: [.init(title: "Acclaimed Professors Teach Virtual Master Classes",
+                                                                               author: "Elizabeth Bottiaux",
+                                                                               imageURL: URL(string: "https://resources.finalsite.net/images/f_auto,q_auto,t_image_size_4/v1620330979/smhsorg/xxqwckx8sjfkrd1iymoa/StudentSoloistsandLiamTeagueApril2021.jpg")!,
+                                                                               articleURL: URL(string: "https://www.smhs.org/about/news/news-stories-details/~board/all-news/post/acclaimed-professors-teach-virtual-master-classes")!)])
+}
