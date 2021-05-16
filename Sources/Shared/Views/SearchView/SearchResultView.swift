90cec8521fcc7dafcacbe5cab17cdb8a2e2de4c9
Jevon Mao <woodburyjevonmao@gmail.com>
2021-05-16 11:05:37 -0700

    Implement search for information cards

--- Sources/Shared/Views/SearchView/SearchResultView.swift
+++ Sources/Shared/Views/SearchView/SearchResultView.swift
@@ -11,7 +11,8 @@ struct SearchResultView: View {
     @Binding var searchText: String
     @State var scheduleWeeks: [ScheduleWeek]
     @State var newsEntries: [NewsEntry]
-    
+    @State var informationCards: [InformationCard]
+    @State var informationCard: InformationCard?
     var body: some View {
         List {
             Section(header: Text("Schedule Days")) {
@@ -25,14 +26,29 @@ struct SearchResultView: View {
                 }
             }
             Section(header: Text("Campus News")) {
-                ForEach(newsEntries.filter{$0.title.lowercased().contains(searchText.lowercased()
-                                                                            .trimmingCharacters(in: .whitespacesAndNewlines))}, id: \.self){news in
+                ForEach(newsEntries
+                            .filter{$0.title.lowercased()
+                                .contains(searchText.lowercased()
+                                            .trimmingCharacters(in: .whitespacesAndNewlines))}, id: \.self){news in
                     let bindingNews = Binding(get: {news}, set: {
                         guard let index = newsEntries.firstIndex(where: {$0.id == news.id}) else {return}
                         newsEntries[index] = $0
                         
                     })
                     NavigationLink(news.title, destination: NewsDetailedView(newsEntry: bindingNews))
+                }
+            }
+            Section(header: Text("Quick Information")) {
+                ForEach(informationCards
+                            .filter{$0.title.lowercased()
+                                .contains(searchText.lowercased()
+                                            .trimmingCharacters(in: .whitespacesAndNewlines))}){card in
+                    Button(card.title) {
+                        informationCard = card
+                    }
+                    .fullScreenCover(item: $informationCard) {
+                        SafariView(url: $0.link).edgesIgnoringSafeArea(.all)
+                    }
                 }
             }
             
