837da273291aca15f89ddde2d06033cb18c17eb1
jevonmao <woodburyjevonmao@gmail.com>
2021-06-25 22:51:06 -0700

    Reduce instances of SharedScheduleInformation

--- Sources/Shared/Views/SearchView/SearchView.swift
+++ Sources/Shared/Views/SearchView/SearchView.swift
@@ -11,7 +11,6 @@ import SwiftUIVisualEffects
 import SwiftlySearch
 
 struct SearchView: View {
-    @StateObject var scheduleViewModel = SharedScheduleInformation()
     @StateObject var newsViewViewModel = NewsViewViewModel()
     @State var searchText: String = ""
     
@@ -24,7 +23,6 @@ struct SearchView: View {
                 }
                 else {
                     SearchResultView(searchText: $searchText,
-                                     scheduleWeeks: scheduleViewModel.scheduleWeeks,
                                      newsEntries: newsViewViewModel.newsEntries,
                                      informationCards: InformationCard.informationCards)
                         .environmentObject(newsViewViewModel)
