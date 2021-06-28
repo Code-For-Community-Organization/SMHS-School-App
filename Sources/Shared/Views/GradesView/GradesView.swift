8c8ab19869feaa655bacbb163f7c6a71179cee12
Jevon Mao <woodburyjevonmao@gmail.com>
2021-06-28 16:51:06 -0700

    Add safeAreaInsets padding to grades view

--- Sources/Shared/Views/GradesView/GradesView.swift
+++ Sources/Shared/Views/GradesView/GradesView.swift
@@ -10,66 +10,61 @@ import SwiftUIVisualEffects
 
 struct GradesView: View {
     @StateObject var gradesViewModel = GradesViewModel()
-    @State private var textDynamicWidth: CGFloat?
+
     var body: some View {
         if gradesViewModel.isLoggedIn {
-            ScrollView {
-                LazyVStack(spacing: 10) {
-                    ForEach(gradesViewModel.gradesResponse.filter{!$0.isPrior}, id: \.self){course in
-                        HStack {
-                            VStack {
-                                Group {
-                                    Text("PERIOD \(course.periodNum)")
-                                        .font(.caption)
-                                        .fontWeight(.medium)
-                                        .foregroundColor(.platformSecondaryLabel)
-                                    Text(course.periodName)
-                                        .font(.title2)
-                                        .lineLimit(1)
-                                        .minimumScaleFactor(0.7)
-                                        .padding(.bottom, 1)
-                                    Text(course.teacherName)
-                                        .font(.subheadline)
-                                        .fontWeight(.semibold)
-                                        .foregroundColor(.platformSecondaryLabel)
+            GeometryReader {geo in
+                ScrollView {
+                    LazyVStack(spacing: 10) {
+                        ForEach(gradesViewModel.gradesResponse.filter{!$0.isPrior}, id: \.self){course in
+                            HStack {
+                                VStack {
+                                    Group {
+                                        Text("PERIOD \(course.periodNum)")
+                                            .font(.caption)
+                                            .fontWeight(.medium)
+                                            .foregroundColor(.platformSecondaryLabel)
+                                        Text(course.periodName)
+                                            .font(.title2)
+                                            .lineLimit(1)
+                                            .minimumScaleFactor(0.7)
+                                            .padding(.bottom, 1)
+                                        Text(course.teacherName)
+                                            .font(.subheadline)
+                                            .fontWeight(.semibold)
+                                            .foregroundColor(.platformSecondaryLabel)
+                                    }
+                                    .frame(maxWidth: .infinity, alignment: .leading)
                                 }
-                                .frame(maxWidth: .infinity, alignment: .leading)
-                            }
-                            Spacer()
-                            Text(course.currentMark)
-                                .font(.title2)
-                                .fontWeight(.bold)
-                            
-                                Text("\(course.gradePercent)%")
+                                Spacer()
+                                Text(course.currentMark)
                                     .font(.title2)
                                     .fontWeight(.bold)
-                                    .frame(width: textDynamicWidth, alignment: .trailing)
-                                    .background(CenteringView())
-                            
-                        }
-                        .padding()
-                        .roundedCorners(cornerRadius: 10)
-                        .background(.platformBackground)
-                    }
-                }
-                .padding(.horizontal)
-            }
-            .overlay(
-                BlurEffect()
-                    .frame(height: UIDevice.hasTopNotch ? 35 : 20)
-                    .frame(maxWidth: .infinity)
-                    .blurEffectStyle(.systemUltraThinMaterial)
-                    .edgesIgnoringSafeArea(.top),
-                alignment: .top)
-            .background(.platformSecondaryBackground)
-            .onPreferenceChange(CenteringColumnPreferenceKey.self) { preferences in
-                        for p in preferences {
-                            let oldWidth = self.textDynamicWidth ?? .zero
-                            if p.width > oldWidth {
-                                self.textDynamicWidth = p.width
+                                
+                                    Text("\(course.gradePercent)%")
+                                        .font(.title2)
+                                        .fontWeight(.bold)
+                                
                             }
+                            .padding()
+                            .background(.platformBackground)
+                            .roundedCorners(cornerRadius: 10)
                         }
                     }
+                    .padding(.horizontal)
+                    .padding(.vertical, geo.safeAreaInsets.top)
+                }
+                .overlay(
+                    BlurEffect()
+                        .frame(height: UIDevice.hasTopNotch ? 35 : 20)
+                        .frame(maxWidth: .infinity)
+                        .blurEffectStyle(.systemUltraThinMaterial)
+                        .edgesIgnoringSafeArea(.top),
+                    alignment: .top)
+                .background(.platformSecondaryBackground)
+                .edgesIgnoringSafeArea(.top)
+            }
+            
         }
         else {
             VStack {
@@ -100,31 +95,5 @@ struct GradesView: View {
 struct GradesView_Previews: PreviewProvider {
     static var previews: some View {
         GradesView()
-    }
-}
-struct CenteringColumnPreferenceKey: PreferenceKey {
-    typealias Value = [CenteringColumnPreference]
-
-    static var defaultValue: [CenteringColumnPreference] = []
-
-    static func reduce(value: inout [CenteringColumnPreference], nextValue: () -> [CenteringColumnPreference]) {
-        value.append(contentsOf: nextValue())
-    }
-}
-
-struct CenteringColumnPreference: Equatable {
-    let width: CGFloat
-}
-
-struct CenteringView: View {
-    var body: some View {
-        GeometryReader { geometry in
-            Rectangle()
-                .fill(Color.clear)
-                .preference(
-                    key: CenteringColumnPreferenceKey.self,
-                    value: [CenteringColumnPreference(width: geometry.frame(in: CoordinateSpace.global).width)]
-                )
-        }
     }
 }
