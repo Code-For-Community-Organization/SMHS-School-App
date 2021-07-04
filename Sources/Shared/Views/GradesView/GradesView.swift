d48424d6dea9abcf2445b33754c9e489f5abb80c
Jevon Mao <woodburyjevonmao@gmail.com>
2021-07-04 16:46:59 -0700

    Redesign login textfields

--- Sources/Shared/Views/GradesView/GradesView.swift
+++ Sources/Shared/Views/GradesView/GradesView.swift
@@ -13,42 +13,38 @@ struct GradesView: View {
     @Namespace private var viewAnimation
     
     var body: some View {
-        GeometryReader {geo in
-            ScrollView {
-                LazyVStack(spacing: 10) {
-                    ForEach(gradesViewModel.gradesResponse.filter{!$0.isPrior}, id: \.self){
-                        CourseGradeItem(course: $0)
-                    }
-                    Button(action: {
-                        withAnimation {
-                            gradesViewModel.signoutAndRemove()
+        NavigationView {
+            if gradesViewModel.isLoggedIn {
+                ScrollView {
+                    LazyVStack(spacing: 10) {
+                        ForEach(gradesViewModel.gradesResponse.filter{!$0.isPrior}, id: \.self){
+                            CourseGradeItem(course: $0)
                         }
-                    }) {
-                        Text("Log out")
-                            .fontWeight(.semibold)
+                        Button(action: {
+                            withAnimation {
+                                gradesViewModel.signoutAndRemove()
+                            }
+                        }) {
+                            Text("Log out")
+                                .fontWeight(.semibold)
+                        }
+                        .padding(.top, 20)
+                        
                     }
-                    .padding(.top, 20)
-                    
+                    .padding(.horizontal)
+                    .padding(.vertical)
                 }
-                .padding(.horizontal)
-                .padding(.vertical, geo.safeAreaInsets.top + 5)
+                .background(.platformSecondaryBackground)
+                .navigationBarTitle("Grades")
+
             }
-            .overlay(
-                BlurEffect()
-                    .frame(height: geo.safeAreaInsets.top)
-                    .frame(maxWidth: .infinity)
-                    .blurEffectStyle(.systemUltraThinMaterial)
-                    .edgesIgnoringSafeArea(.top),
-                alignment: .top)
-            .background(.platformSecondaryBackground)
-            .edgesIgnoringSafeArea(.top)
+            else {
+                GradesLoginView(gradesViewModel: gradesViewModel, animation: viewAnimation)
+                .navigationBarTitle("Grades")
+
+            }
+        
         }
-        .disabled(gradesViewModel.isLoggedIn ? false : true)
-        .opacity(gradesViewModel.isLoggedIn ? 1 : 0)
-        .overlay(
-            GradesLoginView(gradesViewModel: gradesViewModel, animation: viewAnimation)
-                .opacity(gradesViewModel.isLoggedIn ? 0 : 1)
-        )
     }
 }
 
---
+++ Sources/Shared/Views/ViewComponents/ActionButton.swift
@@ -0,0 +1,36 @@
+//
+//  ActionButton.swift
+//  SMHSSchedule
+//
+//  Created by Jevon Mao on 7/4/21.
+//
+
+import SwiftUI
+
+struct ActionButton: View {
+    var label: String
+    var foregroundColor: Color = .white
+    var backgroundColor: Color = .primary
+    var horizontalPadding: CGFloat = 100
+    var action: () -> Void = {}
+    var autoWidth: CGFloat {
+        min(CGFloat(400), UIScreen.screenWidth - horizontalPadding)
+    }
+    var body: some View {
+        Button(action: action) {
+            Text(label)
+                .fontWeight(.semibold)
+                .frame(width: autoWidth)
+                .padding()
+                .background(backgroundColor)
+                .foregroundColor(foregroundColor)
+                .roundedCorners(cornerRadius: 10)
+        }
+    }
+}
+
+struct ActionButton_Previews: PreviewProvider {
+    static var previews: some View {
+        ActionButton(label: "Click Me", action: {})
+    }
+}
