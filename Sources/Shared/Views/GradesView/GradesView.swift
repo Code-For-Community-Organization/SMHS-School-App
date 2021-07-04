450e2c8806bf96b0d977ed32c9a648f969b9f28c
Jevon Mao <woodburyjevonmao@gmail.com>
2021-07-04 15:22:45 -0700

    Optimize textfield UX for grades login

--- Sources/Shared/Views/GradesView/GradesView.swift
+++ Sources/Shared/Views/GradesView/GradesView.swift
@@ -13,45 +13,42 @@ struct GradesView: View {
     @Namespace private var viewAnimation
     
     var body: some View {
-        if gradesViewModel.isLoggedIn {
-            GeometryReader {geo in
-                ScrollView {
-                    LazyVStack(spacing: 10) {
-                        ForEach(gradesViewModel.gradesResponse.filter{!$0.isPrior}, id: \.self){
-                            CourseGradeItem(course: $0)
-                        }
-                        Button(action: {
-                            withAnimation {
-                                gradesViewModel.signoutAndRemove()
-                            }
-                        }) {
-                            Text("Log out")
-                                .fontWeight(.semibold)
-                        }
-                        .padding(.top, 20)
-
+        GeometryReader {geo in
+            ScrollView {
+                LazyVStack(spacing: 10) {
+                    ForEach(gradesViewModel.gradesResponse.filter{!$0.isPrior}, id: \.self){
+                        CourseGradeItem(course: $0)
                     }
-                    .padding(.horizontal)
-                    .padding(.vertical, geo.safeAreaInsets.top)
+                    Button(action: {
+                        withAnimation {
+                            gradesViewModel.signoutAndRemove()
+                        }
+                    }) {
+                        Text("Log out")
+                            .fontWeight(.semibold)
+                    }
+                    .padding(.top, 20)
+                    
                 }
-                .overlay(
-                    BlurEffect()
-                        .frame(height: UIDevice.hasTopNotch ? 35 : 20)
-                        .frame(maxWidth: .infinity)
-                        .blurEffectStyle(.systemUltraThinMaterial)
-                        .edgesIgnoringSafeArea(.top),
-                    alignment: .top)
-                .background(.platformSecondaryBackground)
-                .edgesIgnoringSafeArea(.top)
+                .padding(.horizontal)
+                .padding(.vertical, geo.safeAreaInsets.top + 5)
             }
-            .transition(.slide)
-            
+            .overlay(
+                BlurEffect()
+                    .frame(height: geo.safeAreaInsets.top)
+                    .frame(maxWidth: .infinity)
+                    .blurEffectStyle(.systemUltraThinMaterial)
+                    .edgesIgnoringSafeArea(.top),
+                alignment: .top)
+            .background(.platformSecondaryBackground)
+            .edgesIgnoringSafeArea(.top)
         }
-        else {
+        .disabled(gradesViewModel.isLoggedIn ? false : true)
+        .opacity(gradesViewModel.isLoggedIn ? 1 : 0)
+        .overlay(
             GradesLoginView(gradesViewModel: gradesViewModel, animation: viewAnimation)
-                .transition(.slide)
-        }
-
+                .opacity(gradesViewModel.isLoggedIn ? 0 : 1)
+        )
     }
 }
 
