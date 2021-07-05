9b6c8fe6bd9773f1d93e598bdd06ecf654fcc553
jevonmao <woodburyjevonmao@gmail.com>
2021-07-04 18:23:50 -0700

    Refactor large functions

--- Sources/Shared/Views/CalendarView/MasterCalendarViewModel.swift
+++ Sources/Shared/Views/CalendarView/MasterCalendarViewModel.swift
@@ -13,6 +13,9 @@ class MasterCalendarViewModel: ObservableObject {
     @Published(key: "calendarManager") var calendarManager = CalendarManager()
     @Storage(key: "lastReloadTime", defaultValue: nil) var lastReloadTime: Date?
     
+    //Preferred API for fetching calendar data
+    //Because networking is resource intensive,
+    //this make sure that reload is not too frequent
     func reloadData(completion: @escaping () -> ()) {
         if let time = lastReloadTime, !calendarManager.days.isEmpty {
             completion()
@@ -26,24 +29,40 @@ class MasterCalendarViewModel: ObservableObject {
             fetchData{completion()}
         }
     }
+    
+    func getInBetweenMonths(forStart startDate: Date, endDate: Date) -> [String]? {
+        //Compute dates of each month in bewteen 2 given dates
+        computeMonths: do {
+            var inBetweenMonths = [String]()
+            let months = endDate.months(from: startDate)
+            guard endDate > startDate else {return nil}
+            for i in 0...months {
+                var dateComponents = Calendar.current.dateComponents([.year, .month, .day], from: startDate)
+                dateComponents.month = dateComponents.month?.advanced(by: i)
+                dateComponents.day = 1
+                let date = Calendar.current.date(from: dateComponents)
+                guard let date = date else { return nil }
+                let dateFormatter = DateFormatter()
+                dateFormatter.dateFormat = "yyyy-MM-dd"
+                let formattedDate = dateFormatter.string(from: date)
+                inBetweenMonths.append(formattedDate)
+            }
+            return inBetweenMonths
+        }
+        
+    }
+    //- Parameters:
+    //startDate: The starting range of dates to fetch calendar information for
+    //endDate: The ending range of dates to fetch calendar information for
+    //completion: Handler to notify when networking is finished
     func fetchData(from startDate: Date = Date(),
                    to endDate: Date = Calendar.current.date(byAdding: .month, value: 3, to: Date())!,
                    completion: @escaping () -> ()) {
-        print("Fetching calendar data")
-        let months = endDate.months(from: startDate)
-        var inBetweenMonths = [String]()
-        guard endDate > startDate else {return}
-        for i in 0...months {
-            var dateComponents = Calendar.current.dateComponents([.year, .month, .day], from: startDate)
-            dateComponents.month = dateComponents.month?.advanced(by: i)
-            dateComponents.day = 1
-            let date = Calendar.current.date(from: dateComponents)
-            guard let date = date else {return}
-            let dateFormatter = DateFormatter()
-            dateFormatter.dateFormat = "yyyy-MM-dd"
-            let formattedDate = dateFormatter.string(from: date)
-            inBetweenMonths.append(formattedDate)
-        }
+    
+        guard let inBetweenMonths = getInBetweenMonths(forStart: startDate,
+                                                       endDate: endDate)
+        else { return }
+    
         let dispatchGroup = DispatchGroup()
         for month in inBetweenMonths {
             dispatchGroup.enter()
@@ -60,54 +79,59 @@ class MasterCalendarViewModel: ObservableObject {
                     #endif
                     return
                 }
-                do {
-                    let html = try SwiftSoup.parse(htmlText)
-                    let daysElement = try html.select("div.fsCalendarDaybox.fsStateHasEvents:not(.fsCalendarOutOfRange)")
-                    for day in daysElement {
-                        let childrenElement = day.children()
-                        let dateString = "\(try childrenElement.attr("data-year"))-\(Int(try childrenElement.attr("data-month"))!+1)-\(try childrenElement.attr("data-day"))"
-                        let dateFormatter = DateFormatter()
-                        dateFormatter.dateFormat = "yyyy-M-d"
-                        let formattedDate = dateFormatter.date(from: dateString)!
-                        var events = [CalendarEvent]()
-                        for event in try day.select("div.fsCalendarInfo") {
-                            let eventTitle = try event.select("a.fsCalendarEventTitle.fsCalendarEventLink").text()
-                            let timeRange = try event.select("div.fsTimeRange")
-                            let startTime = try timeRange.select("time.fsStartTime").attr("datetime")
-                            let endTime = try timeRange.select("time.fsEndTime").attr("datetime")
-                            let dateFormatter = DateFormatter()
-                            dateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ssZ"
-                            guard !eventTitle.isEmpty else {continue}
-                            guard let startTime = dateFormatter.date(from: startTime),
-                                  let endTime = dateFormatter.date(from: endTime) else {
-                                events.append(.init(title: eventTitle, isFullDay: true))
-                                continue
-                            }
-                            events.append(.init(title: eventTitle,
-                                                startTime: startTime,
-                                                endTime: endTime))
-                        }
-                        let day = CalendarDay(date: formattedDate, events: events)
-                        DispatchQueue.main.async {
-                            self.calendarManager.days[day.date] = day
-                        }
-                    }
-                }
-                catch Exception.Error(type: let type, Message: let message) {
-                    #if DEBUG
-                    print("Error of type \(type), \(message)")
-                    #endif
-                }
-                catch {
-                    #if DEBUG
-                    print("Unknown error")
-                    #endif
-                }
+                self.parseCalendarFromHTML(htmlText)
                 dispatchGroup.leave()
             }
         }
         dispatchGroup.notify(queue: .main) {
             completion()
+        }
+    }
+    
+    func parseCalendarFromHTML(_ htmlText: String) {
+        do {
+            let html = try SwiftSoup.parse(htmlText)
+            let css = "div.fsCalendarDaybox.fsStateHasEvents:not(.fsCalendarOutOfRange)"
+            let daysElement = try html.select(css)
+            for day in daysElement {
+                let childrenElement = day.children()
+                let dateString = "\(try childrenElement.attr("data-year"))-\(Int(try childrenElement.attr("data-month"))!+1)-\(try childrenElement.attr("data-day"))"
+                let dateFormatter = DateFormatter()
+                dateFormatter.dateFormat = "yyyy-M-d"
+                let formattedDate = dateFormatter.date(from: dateString)!
+                let events: [CalendarEvent] = try day.select("div.fsCalendarInfo")
+                    .compactMap {event in
+                        let eventTitle = try event.select("a.fsCalendarEventTitle.fsCalendarEventLink").text()
+                        let timeRange = try event.select("div.fsTimeRange")
+                        let startTime = try timeRange.select("time.fsStartTime").attr("datetime")
+                        let endTime = try timeRange.select("time.fsEndTime").attr("datetime")
+                        let dateFormatter = DateFormatter()
+                        dateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ssZ"
+                        guard !eventTitle.isEmpty else { return nil }
+                        guard let startTime = dateFormatter.date(from: startTime),
+                              let endTime = dateFormatter.date(from: endTime) else {
+                            return .init(title: eventTitle, isFullDay: true)
+                        }
+                        return .init(title: eventTitle,
+                                            startTime: startTime,
+                                            endTime: endTime)
+                    }
+                
+                let day = CalendarDay(date: formattedDate, events: events)
+                DispatchQueue.main.async {
+                    self.calendarManager.days[day.date] = day
+                }
+            }
+        }
+        catch Exception.Error(type: let type, Message: let message) {
+            #if DEBUG
+            print("Error of type \(type), \(message)")
+            #endif
+        }
+        catch {
+            #if DEBUG
+            print("Unknown error")
+            #endif
         }
     }
 }
