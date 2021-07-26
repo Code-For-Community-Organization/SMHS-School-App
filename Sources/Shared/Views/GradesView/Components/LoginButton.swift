310a66dd3da24dad3da48dfcb74e17269c784796
Jevon Mao <woodburyjevonmao@gmail.com>
2021-07-26 12:28:08 -0700

    Error handling and displaying for log in

---
+++ Sources/Shared/Views/GradesView/Components/LoginButton.swift
@@ -0,0 +1,41 @@
+//
+//  LoginButton.swift
+//  SMHSSchedule (iOS)
+//
+//  Created by Jevon Mao on 7/26/21.
+//
+
+import SwiftUI
+
+struct LoginButton: View {
+    @ObservedObject var gradesViewModel: GradesViewModel
+    
+    var body: some View {
+        Button(action: {
+            withAnimation {
+                gradesViewModel.loginAndFetch()
+            }
+        }){
+            ZStack {
+                if gradesViewModel.isLoading {
+                    ProgressView().progressViewStyle(CircularProgressViewStyle())
+                        .foregroundColor(.white)
+                        .padding()
+                }
+                else {
+                    Text("Log In")
+                        .fontWeight(.semibold)
+                        .padding(10)
+                }
+            }
+            .frame(width: min(CGFloat(400), UIScreen.screenWidth - 100))
+            .background(.primary)
+            .foregroundColor(.platformBackground)
+            .roundedCorners(cornerRadius: 10)
+     
+        }
+        .disabled(gradesViewModel.isLoading)
+        .padding(.bottom)
+    }
+}
+
