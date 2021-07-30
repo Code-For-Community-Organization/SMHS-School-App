19e09a43bf66fe8cee6261bfae7109ce577ba0b6
Jevon Mao <woodburyjevonmao@gmail.com>
2021-07-30 12:14:24 -0700

    Automatic grades reload and animation

--- Sources/Shared/Views/GradesView/Components/GradesLoginView.swift
+++ Sources/Shared/Views/GradesView/Components/GradesLoginView.swift
@@ -12,58 +12,42 @@ struct GradesLoginView: View {
     @ObservedObject var gradesViewModel: GradesViewModel
     
     var body: some View {
-        ZStack {
-            VStack {
-                Form {
-                    Section(header: Text("Aeries Gradebook Email"),
-                            footer: Text(gradesViewModel.emailErrorMsg)
-                                        .lineLimit(nil)
-                                        .foregroundColor(.red)) {
-                        TextField("School email", text: $gradesViewModel.email)
-                            .autocapitalization(.none)
-                            .disableAutocorrection(true)
-                            .keyboardType(.emailAddress)
-                            .introspectTextField { textfield in
-                                textfield.returnKeyType = .go
-                            }
-                        
-                    }
-                    Section(header: Text("Aeries Gradebook Password"),
-                            footer: Text(gradesViewModel.passwordErrorMsg)
-                                        .lineLimit(nil)
-                                        .foregroundColor(.red)) {
-                        SecureField("Password", text: $gradesViewModel.password)
-                            .introspectTextField { textfield in
-                                textfield.returnKeyType = .go
-                            }
-                    }
+        VStack {
+            Form {
+                Section(header: Text("Aeries Gradebook Email"),
+                        footer: Text(gradesViewModel.emailErrorMsg)
+                            .lineLimit(nil)
+                            .foregroundColor(.red)) {
+                    TextField("School email", text: $gradesViewModel.email)
+                        .autocapitalization(.none)
+                        .disableAutocorrection(true)
+                        .keyboardType(.emailAddress)
+                        .introspectTextField { textfield in
+                            textfield.returnKeyType = .go
+                        }
+                    
                 }
-                LoginButton(gradesViewModel: gradesViewModel)
-                    .animation(nil)
-                    .opacity(gradesViewModel.isValid ? 1 : 0.3)
-                    .disabled(gradesViewModel.isValid ? false : true)
-                
+                Section(header: Text("Aeries Gradebook Password"),
+                        footer: Text(gradesViewModel.passwordErrorMsg)
+                            .lineLimit(nil)
+                            .foregroundColor(.red)) {
+                    SecureField("Password", text: $gradesViewModel.password)
+                        .introspectTextField { textfield in
+                            textfield.returnKeyType = .go
+                        }
+                }
             }
-            .blur(radius: gradesViewModel.isLoading ? 20 : 0)
+            LoginButton(gradesViewModel: gradesViewModel)
+                .animation(nil)
+                .opacity(gradesViewModel.isValid ? 1 : 0.3)
+                .disabled(gradesViewModel.isValid ? false : true)
             
-            if gradesViewModel.isLoading {
-                ProgressView().progressViewStyle(CircularProgressViewStyle())
-                    .foregroundColor(.white)
-                    .padding(50)
-                    .background(.platformSecondaryFill)
-                    .clipShape(RoundedRectangle(cornerRadius: 10, style: .continuous))
-                    .shadow(color: Color.black.opacity(0.3), radius: 10, x: 0, y: 5)
-                    .transition(AnyTransition.opacity.combined(with: .slide))
-            }
-         
         }
-        .animation(.easeInOut)
         .alert(isPresented: $gradesViewModel.showNetworkError) {
             Alert(title: Text(gradesViewModel.networkErrorTitle),
                   message: Text(gradesViewModel.networkErrorMsg),
                   dismissButton: .cancel())
         }
-        .disabled(gradesViewModel.isLoading ? true : false)
     }
 }
 
@@ -71,12 +55,12 @@ struct LoginTextFieldRoundedStyle: TextFieldStyle {
     @Binding var focused: Bool
     
     func _body(configuration: TextField<Self._Label>) -> some View {
-            configuration
+        configuration
             .padding(.horizontal, 20)
             .padding(.vertical, 15)
             .background(
                 RoundedRectangle(cornerRadius: 8, style: .continuous)
                     .stroke(focused ? Color.primary : .gray, lineWidth: 3)
             ).padding()
-        }
+    }
 }
--- Sources/Shared/Views/GradesView/GradesView.swift
+++ Sources/Shared/Views/GradesView/GradesView.swift
@@ -6,45 +6,60 @@
 //
 
 import SwiftUI
+import SwiftUIVisualEffects
 
 struct GradesView: View {
     @StateObject var gradesViewModel = GradesViewModel()
-    @State var isRefreshing = false
     
     var body: some View {
         NavigationView {
-            if !gradesViewModel.gradesResponse.isEmpty {
-                ScrollView {
-                    LazyVStack(spacing: 20) {
-                        ForEach(gradesViewModel.gradesResponse.filter{!$0.isPrior}, id: \.self){
-                            CourseGradeItem(course: $0)
-                        }
-                        ActionButton(label: "Log Out") {
-                            withAnimation {
-                                gradesViewModel.showLogoutAlert = true
+            ZStack {
+                if !gradesViewModel.gradesResponse.isEmpty {
+                    ScrollView {
+                        LazyVStack(spacing: 20) {
+                            ForEach(gradesViewModel.gradesResponse.filter{!$0.isPrior}, id: \.self){
+                                CourseGradeItem(course: $0)
+                            }
+                            ActionButton(label: "Log Out") {
+                                withAnimation {
+                                    gradesViewModel.showLogoutAlert = true
+                                }
+                            }
+                            .padding(.vertical, 20)
+                            .alert(isPresented: $gradesViewModel.showLogoutAlert) {
+                                Alert(title: Text("Confirm Log out?"),
+                                      message: Text(GradesViewModel.logoutDescription),
+                                      primaryButton: .destructive(Text("Log Out"),
+                                                                  action: {gradesViewModel.signoutAndRemove()}),
+                                      secondaryButton: .cancel())
                             }
                         }
-                        .padding(.vertical, 20)
-                        .alert(isPresented: $gradesViewModel.showLogoutAlert) {
-                            Alert(title: Text("Confirm Log out?"),
-                                  message: Text(GradesViewModel.logoutDescription),
-                                  primaryButton: .destructive(Text("Log Out"),
-                                                              action: {gradesViewModel.signoutAndRemove()}),
-                                  secondaryButton: .cancel())
-                        }
+                        .padding(.horizontal)
+                        .padding(.vertical)
                     }
-                    .padding(.horizontal)
-                    .padding(.vertical)
+                    .navigationBarTitle("Grades")
+                    .onAppear(perform: gradesViewModel.reloadData)
+                    .blur(radius: gradesViewModel.isLoading ? 20 : 0)
                 }
-                .navigationBarTitle("Grades")
+                else {
+                    GradesLoginView(gradesViewModel: gradesViewModel)
+                    .navigationBarTitle("Grades")
+                    .blur(radius: gradesViewModel.isLoading ? 20 : 0)
+                }
 
+                if gradesViewModel.isLoading {
+                    ProgressView().progressViewStyle(CircularProgressViewStyle())
+                        .foregroundColor(.white)
+                        .padding(50)
+                        .background(BlurEffect())
+                        .blurEffectStyle(.systemThinMaterial)
+                        .clipShape(RoundedRectangle(cornerRadius: 10, style: .continuous))
+                        .shadow(color: Color.black.opacity(0.15), radius: 10, x: 0, y: 5)
+                        .transition(AnyTransition.opacity.combined(with: .scale))
+                }
             }
-            else {
-                GradesLoginView(gradesViewModel: gradesViewModel)
-                .navigationBarTitle("Grades")
-
-            }
-        
+            .animation(.easeInOut)
+            .disabled(gradesViewModel.isLoading ? true : false)
         }
     }
     
