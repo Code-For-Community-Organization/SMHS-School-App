0c3c35be3a067e95c8d0f7f5174b8d8ed21a2c3f
Jevon Mao <woodburyjevonmao@gmail.com>
2022-07-23 21:27:07 -0700

    fix preview sample showing duplicate schedule

--- Sources/Shared/Views/ScheduleView/Model/ScheduleDay.swift
+++ Sources/Shared/Views/ScheduleView/Model/ScheduleDay.swift
@@ -30,7 +30,7 @@ struct ScheduleDay: Hashable, Identifiable, Codable {
         return date.hashValue
     }
     var dayOfTheWeek: Int {
-        Date.getDayOfTheWeek(for: date)
+        Date.getDayOfTheWeek(for: mockDate ?? date)
     }
     var date: Date  //Date of the schedule
     var scheduleText: String
@@ -108,7 +108,8 @@ extension ScheduleDay {
             customPeriods[periodIndex] = newValue
         }
     }
-    
-    static let sampleScheduleDay = ScheduleDay(date: Date(), scheduleText: "Period 6 8:00-9:05\nPeriod 7 9:12-10:22\n(5 minutes for announcements)\nNutrition                      Period 1\n10:22-11:02                10:29-11:34\nPeriod 1                        Nutrition\n11:09-12:14                 11:34-12:14\nPeriod 2 12:21-1:26\nOffice Hours 1:33-2:30\n-------------------------------\nAP French Lang/Culture & Modern World Hist 8:00\nAP Macroeconomics 12:00\nB FS Golf vs MD 3:30\nB JV Golf @ JSerra 3:00\nB JV Tennis vs Servite 3:15\nB JV/V LAX @ JSerra 7:00/5:30\nB V Tennis @ Servite 2:30\nB V Vball @ JSerra 3:00\nG JV/V LAX @ Orange Luth 7:00/5:30\nG V Golf vs Rosary 4:30\nPossible G Soccer CIF\nSenior Graduation Ticket Distribution\n\nV Wrestling vs Aliso Niguel 1:30\n")
+
+    static let schoolDay = DateFormatter().serverTimeFormat("2022-07-22T04:24:14+0000")!
+    static let sampleScheduleDay = ScheduleDay(date: schoolDay, scheduleText: "Period 6 8:00-9:05\nPeriod 7 9:12-10:22\n(5 minutes for announcements)\nNutrition                      Period 1\n10:22-11:02                10:29-11:34\nPeriod 1                        Nutrition\n11:09-12:14                 11:34-12:14\nPeriod 2 12:21-1:26\nOffice Hours 1:33-2:30\n-------------------------------\nAP French Lang/Culture & Modern World Hist 8:00\nAP Macroeconomics 12:00\nB FS Golf vs MD 3:30\nB JV Golf @ JSerra 3:00\nB JV Tennis vs Servite 3:15\nB JV/V LAX @ JSerra 7:00/5:30\nB V Tennis @ Servite 2:30\nB V Vball @ JSerra 3:00\nG JV/V LAX @ Orange Luth 7:00/5:30\nG V Golf vs Rosary 4:30\nPossible G Soccer CIF\nSenior Graduation Ticket Distribution\n\nV Wrestling vs Aliso Niguel 1:30\n")
 }
 
