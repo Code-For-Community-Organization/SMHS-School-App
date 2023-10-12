557c5763552215a59c58081a729173bccf47b82c
Jevon Mao <woodburyjevonmao@gmail.com>
2023-10-11 18:05:32 -0700

    Fix firebase notification
    Disable method swizzling, typo in team ID in console

--- Sources/SMHS (iOS)/AppCycle/AppDelegate.swift
+++ Sources/SMHS (iOS)/AppCycle/AppDelegate.swift
@@ -6,21 +6,35 @@
 //
 
 import SwiftUI
-import Firebase
+import FirebaseCore
 import FirebaseRemoteConfig
 import FirebaseMessaging
+import FirebaseCrashlytics
 
 final class AppDelegate: NSObject, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
         // Configure Firebase Suite
         FirebaseApp.configure()
+        FirebaseConfiguration.shared.setLoggerLevel(.min)
+
+        UNUserNotificationCenter.current().delegate = self
+
+        // Request authorization for push notifications
+        let authOptions: UNAuthorizationOptions = [.alert, .badge, .sound]
+        UNUserNotificationCenter.current().requestAuthorization(
+            options: authOptions,
+            completionHandler: { _, _ in }
+        )
+        application.registerForRemoteNotifications()
+
         setupFirebaseRemoteConfig()
-        setupPushNotifications()
-        setupFirebaseMessaging()
+
         #if DEBUG
         Crashlytics.crashlytics().setCrashlyticsCollectionEnabled(false)
         #endif
+        // Configure notification delegates
+        Messaging.messaging().delegate = self
         return true
     }
 
@@ -33,20 +47,6 @@ final class AppDelegate: NSObject, UIApplicationDelegate {
 
 // MARK: - UNUserNotification
 extension AppDelegate: UNUserNotificationCenterDelegate {
-    // Helper method
-    func setupPushNotifications() {
-        UNUserNotificationCenter.current().delegate = self
-
-        // Request authorization for push notifications
-        let authOptions: UNAuthorizationOptions = [.alert, .badge, .sound]
-        UNUserNotificationCenter.current().requestAuthorization(
-            options: authOptions,
-            completionHandler: { _, _ in }
-        )
-        DispatchQueue.main.async {
-            UIApplication.shared.registerForRemoteNotifications()
-        }
-    }
     // MARK: Delegate methods
     // Receive displayed notifications for iOS 10 devices
     // Called for when app is in background
@@ -54,6 +54,9 @@ extension AppDelegate: UNUserNotificationCenterDelegate {
                                 willPresent notification: UNNotification,
                                 withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions)
                                 -> Void) {
+        let userInfo = notification.request.content.userInfo
+
+        Messaging.messaging().appDidReceiveMessage(userInfo)
         // Change this to your preferred presentation option
         completionHandler([.banner, .sound, .list])
     }
@@ -62,17 +65,45 @@ extension AppDelegate: UNUserNotificationCenterDelegate {
     func userNotificationCenter(_ center: UNUserNotificationCenter,
                                 didReceive response: UNNotificationResponse,
                                 withCompletionHandler completionHandler: @escaping () -> Void) {
+        let userInfo = response.notification.request.content.userInfo
+
+        Messaging.messaging().appDidReceiveMessage(userInfo)
         completionHandler()
     }
+
+    func application(_ application: UIApplication, didReceiveRemoteNotification userInfo: [AnyHashable : Any], fetchCompletionHandler completionHandler: @escaping (UIBackgroundFetchResult) -> Void) {
+        Messaging.messaging().appDidReceiveMessage(userInfo)
+        completionHandler(.noData)
+    }
+
+    func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+    debugPrint("Device notification token: \(deviceToken.map { String(format: "%02.2hhx", $0) }.joined())")
+      Messaging.messaging().apnsToken = deviceToken
+    }
+
+    func application(_ application: UIApplication, didFailToRegisterForRemoteNotificationsWithError error: Error) {
+        debugPrint(error)
+    }
+
+
 }
 
 // MARK: Firebase Messaging
 extension AppDelegate: MessagingDelegate {
-    // Helper method
-    func setupFirebaseMessaging() {
-        Messaging.messaging().delegate = self
-    }
+    // MARK: Delegate methods
+    func messaging(_ messaging: Messaging, didReceiveRegistrationToken fcmToken: String?) {
+        debugPrint("FCM Token: \(fcmToken ?? "")")
 
+        let dataDict: [String: String] = ["token": fcmToken ?? ""]
+        NotificationCenter.default.post(
+          name: Notification.Name("FCMToken"),
+          object: nil,
+          userInfo: dataDict
+        )
+    }
+}
+
+extension AppDelegate {
     // Helper method, intializes Remote Config
     func setupFirebaseRemoteConfig() {
         // Remote config fetch
@@ -101,10 +132,5 @@ extension AppDelegate: MessagingDelegate {
         }
         Constants.remoteConfig.configSettings.minimumFetchInterval = TimeInterval(Constants.minimumFetchInterval)
 
-    }
-
-    // MARK: Delegate methods
-    func messaging(_ messaging: Messaging, didReceiveRegistrationToken fcmToken: String?) {
-        debugPrint("FCM Token: \(fcmToken ?? "")")
     }
 }
